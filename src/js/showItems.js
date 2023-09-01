3f9485f658ec7a34c90ed0ba73919b05b17d915e
andrsweb <proflinervk@gmail.com>
2023-09-02 05:05:50 +0700

    create artist

--- src/js/showItems.js
+++ src/js/showItems.js
@@ -1,17 +1,17 @@
 document.addEventListener( 'DOMContentLoaded', () => {
 	'use strict'
 
-	showMoreCards( '.show__more' )
+	showMoreCards( '.show__more', '.event:not(.visible)' )
+	showMoreCards( '.show__more', '.coming__event:not(.visible)' )
 } )
 
-const showMoreCards = ( button ) => {
+const showMoreCards = ( button, selector ) => {
 	const cardsButton = document.querySelector( button )
+	const cards = document.querySelectorAll(selector)
 
 	if ( ! cardsButton ) return
 
 	cardsButton.addEventListener( 'click', () => {
-		const cards = document.querySelectorAll( '.event:not(.visible)' )
-
 		if ( ! cards.length ) {
 			cardsButton.remove()
 			return
